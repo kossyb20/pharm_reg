71c8aa9fae61a407f999df4de630f966b78fd1d4
Kossy <kossyb20@gmail.com>
2019-10-16 09:37:00 +0100

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,75 +1,40 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-'use strict';
 
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
 
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-    
-    'js/app.js',
-    'js/pouchdb.min.js',
-    'css/app.css',
-    'index.html',
-];
+const version = new Date().getTime();
+const cachePrefix = 'pharm-reg-';
+const staticCacheName = `${cachePrefix}static-${version}`;
+const expectedCaches = [staticCacheName];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-  // CODELAB: Precache static resources here.
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline assets');
-        return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(staticCacheName);
 
-  self.skipWaiting();
+    await cache.addAll([
+      './',
+      './images/speedometer.png',
+      './js/app.js',
+      './js/pouchdb.min.js',
+      './css/app.css',
+      './img/icon.png',
+      './index.html',
+    ]);
+
+    self.skipWaiting();
+  })());
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-    evt.waitUntil(
-        caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-            }
-        }));
-        })
-    );
-
-  self.clients.claim();
+addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    for (const cacheName of await caches.keys()) {
+      if (!cacheName.startsWith(cachePrefix)) continue;
+      if (!expectedCaches.includes(cacheName)) await caches.delete(cacheName);
+    }
+  })());
 });
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-
-    evt.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(evt.request)
-            .then((response) => {
-                return response || fetch(evt.request);
-            });
-        })
-    );
+addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
+  );
 });
